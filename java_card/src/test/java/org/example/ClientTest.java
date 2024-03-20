dbd7244c1e7dd72fe9cf8ef26d408ff53ad77d88
Artem <artx52135@gmail.com>
2024-03-20 16:59:29 +0300

    add BankCardTests

--- java_card/src/test/java/org/example/ClientTest.java
+++ java_card/src/test/java/org/example/ClientTest.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -31,17 +33,25 @@ public class ClientTest {
         try {
             BufferedWriter writer = client.getWriter();
             assertNotNull(writer);
-            writer.write("Test");
-            writer.flush();
             writer.close();
             assertTrue(new File("Petr Petrov.txt").exists());
         } catch (IOException e) {
             fail("IOException should not be thrown");
         }
    }
+    private void clearFile(String filePath) {
+        try {
+            FileWriter fileWriter = new FileWriter(filePath);
+            fileWriter.write("");
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void testGetWriterFileContent() {
+        clearFile("C:\\JAVA\\java_card\\java_card\\Petr Petrov.txt"); // Очистка файла перед чтением
         try {
             BufferedWriter writer = client.getWriter();
             writer.write("Test");
@@ -56,4 +66,5 @@ public class ClientTest {
             fail("IOException should not be thrown");
         }
     }
+
 }
